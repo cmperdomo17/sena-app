faa3eb008d2ca3884059c779c5a43c664a4eefe1
CrisQR <cdqr27@gmail.com>
2024-05-09 19:37:10 -0500

    ambient change

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import e from 'cors';
 import { LoginComponent } from './components/pages/login/login.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { EnvironmentManagementComponent } from './components/pages/environment-management/environment-management.component';
+import { AmbientsManagementComponent } from './components/pages/ambients-management/ambients-management.component';
 import { TeachersManagementComponent } from './components/pages/teachers-management/teachers-management.component';
 import { ProgramsManagementComponent } from './components/pages/programs-management/programs-management.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'ambients', component: EnvironmentManagementComponent},
+  {path: 'ambients', component: AmbientsManagementComponent},
   {path: 'teachers', component: TeachersManagementComponent},
   {path: 'programs', component: ProgramsManagementComponent}
 ];
