54b380bfab90c986401d8426420da2a22f77b80e
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-05-16 16:43:12 -0500

    Add teacher_fullname property to Teacher interface and form component

--- frontend/src/app/components/pages/form-schedule/form-schedule.component.ts
+++ frontend/src/app/components/pages/form-schedule/form-schedule.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Competence } from '../../../models/Compentence';
+import { Program } from '../../../models/Program';
+import { CompetenciesService } from '../../../services/competencies.service';
+import { ProgramsService } from '../../../services/programs.service';
+import { PeriodsService } from '../../../services/periods.service';
+import { TeachersService } from '../../../services/teachers.service';
+import { Teacher } from '../../../models/Teachers';
+import { Period } from '../../../models/Period';
 
 @Component({
   selector: 'app-form-schedule',
   templateUrl: './form-schedule.component.html',
   styleUrl: './form-schedule.component.css'
 })
-export class FormScheduleComponent {
 
+export class FormScheduleComponent implements OnInit {
+  constructor(private competenciesService: CompetenciesService, private programsService: ProgramsService, private periodsService: PeriodsService, private teachersService: TeachersService) {}
+
+  ngOnInit(): void {
+    this.listPrograms();
+    this.listCompetences();
+  }
+
+  competence: Competence = {
+    competence_id: 0,
+    program_id: null,
+    competence_name: '',
+    competence_state: 0
+  };
+
+  program: Program = {
+    program_id: 0,
+    program_name: '',
+    program_state: 0
+  };
+
+  period: Period = {
+    period_id: 0,
+    period_name: '',
+    period_start_date: '',
+    period_end_date: '',
+    period_state: 0
+  };
+
+  teacher: Teacher = {
+    teacher_id: 0,
+    teacher_name: '',
+    teacher_lastname: '',
+    teacher_fullname: '',
+    teacher_dnitype: '',
+    teacher_dni: '',
+    teacher_type: '',
+    teacher_contracttype: '',
+    teacher_area: '',
+    user_login: '',
+    user_pwd: ''
+  }
+
+  CompetencesList: any = [];
+  ProgramsList: any = [];
+  selectedOption: string = '';
+  @Input() value: string = '';
+
+  selectedTeacher: Teacher = {
+    teacher_id: 0,
+    teacher_name: '',
+    teacher_lastname: '',
+    teacher_fullname: '',
+    teacher_dnitype: '',
+    teacher_dni: '',
+    teacher_type: '',
+    teacher_contracttype: '',
+    teacher_area: '',
+    user_login: '',
+    user_pwd: ''
+  };
+
+  onTeacherSelected(teacher: Teacher) {
+    this.selectedTeacher = teacher;
+  }
+
+  onSelectionChange(selection: string): void {
+    this.selectedOption = selection;
+  }
+
+  listCompetences(): void {
+    this.competenciesService.listCompetencies().subscribe(
+      res => {
+        this.CompetencesList = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  listPrograms(): void {
+    this.programsService.listPrograms().subscribe(
+      res => {
+        this.ProgramsList = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
+
+
--- frontend/src/app/components/pages/schedule-management/schedule-management.component.ts
+++ frontend/src/app/components/pages/schedule-management/schedule-management.component.ts
@@ -1,9 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { PeriodsService } from '../../../services/periods.service';
+import { TeachersService } from '../../../services/teachers.service';
+import { Period } from '../../../models/Period';
+import { Teacher } from '../../../models/Teachers';
 
 @Component({
   selector: 'app-schedule-management',
   templateUrl: './schedule-management.component.html',
   styleUrl: './schedule-management.component.css'
 })
-export class ScheduleManagementComponent {
+export class ScheduleManagementComponent implements OnInit {
+
+  constructor(private periodsService: PeriodsService, private teachersService: TeachersService) { }
+
+  ngOnInit(): void {
+    this.listPeriods();
+    this.listTeachers();
+  }
+
+  period: Period = {
+    period_id: 0,
+    period_name: '',
+    period_start_date: '',
+    period_end_date: '',
+    period_state: 0
+  };
+
+  teacher: Teacher = {
+    teacher_id: 0,
+    teacher_name: '',
+    teacher_lastname: '',
+    teacher_fullname: '',
+    teacher_dnitype: '',
+    teacher_dni: '',
+    teacher_type: '',
+    teacher_contracttype: '',
+    teacher_area: '',
+    user_login: '',
+    user_pwd: ''
+  }
+
+  PeriodsList: any = [];
+  TeachersList: any = [];
+  selectedOption: string = '';
+  teacherFullName: string = '';
+  
+  onSelectionChange(selection: string): void {
+    this.selectedOption = selection;
+  }
+
+  listPeriods(): void {
+    this.periodsService.listPeriods().subscribe(
+      res => {
+        this.PeriodsList = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  listTeachers(): void {
+    this.teachersService.listTeachers().subscribe(
+      res => {
+        this.TeachersList = res;
+        this.TeachersList.forEach((teacher: Teacher) => {
+          teacher.teacher_fullname = teacher.teacher_name + ' ' + teacher.teacher_lastname;
+        });
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
 }
