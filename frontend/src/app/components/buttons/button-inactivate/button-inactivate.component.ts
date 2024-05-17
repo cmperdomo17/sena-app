ebdc272abf89aa99c84f98d0574fc32efe50677d
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-05-17 01:04:32 -0500

    Update button inactivate

--- frontend/src/app/components/buttons/button-inactivate/button-inactivate.component.ts
+++ frontend/src/app/components/buttons/button-inactivate/button-inactivate.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-inactivate',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './button-inactivate.component.css'
 })
 export class ButtonInactivateComponent {
-
+  @Input() isActivate: boolean = false;
 }
